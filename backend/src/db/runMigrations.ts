e0eabc11d6d220fab97e1fec5701ad95ff5088d6
Simon-Mrc <simon.mrc.0@gmail.com>
2026-03-17 11:33:07 +0100

    Working on feature // rename and usermail done // need work on UX and UI (prompts ...)// New migrations 007 add unique check on mail for users // gonna do password change after so i need new route //

--- backend/src/db/runMigrations.ts
+++ backend/src/db/runMigrations.ts
@@ -5,6 +5,7 @@ import { up as up003 } from './migrations/003_removeCreatorColor_schema';
 import { up as up004 } from './migrations/004_add_column_item_schema';
 import { up as up005 } from './migrations/005_add_column_users_avatarFilePath_schema';
 import { up as up006 } from './migrations/006_add_column_users_avatarFilePath_schema';
+import { up as up007 } from './migrations/007_add_unique_mail_users_schema';
 
 interface Migration {
     name : string;
@@ -17,7 +18,8 @@ const allMigration : Migration[] = [
     {name : '003_removeCreatorColor_schema.ts', up : up003},
     {name : '004_add_column_item_schema' , up : up004},
     {name : '005_add_column_users_avatarFilePath_schema' , up : up005},
-    {name : '006_add_column_users_avatarFilePath_schema' , up : up006}
+    {name : '006_add_column_users_avatarFilePath_schema' , up : up006},
+    {name : 'migrations/007_add_unique_mail_users_schema', up : up007}
 ];
 
 export function runMigration(db : InstanceType<typeof Database>){
--- src/components/layout/AccountPart.tsx
+++ src/components/layout/AccountPart.tsx
@@ -16,7 +16,7 @@ export function AccountPart():JSX.Element{
                 <img src={`../backend/${context?.user?.avatarFilePath}`}></img>
               </div>
               <div className="user-info">
-                <span id="userNameDisplay" className="user-name">{context?.user?.name ?? "?"}</span>
+                <span id="userNameDisplay" className="user-name">{context?.user?.userName ?? "?"}</span>
                 <span id="userTypeDisplay" className="user-type"></span>
               </div>
                 <button id="accountSetting" onClick={()=> setSettingMenu(true)}>⚙️Account Settings</button>
@@ -34,20 +34,94 @@ export function AccountPart():JSX.Element{
 export function SettingMenu ({onClose} : {onClose : ()=>void}) : JSX.Element{
 
 //// CHANGES USERNAME /// CHANGE COLOR /// CHANGE MAIL ? /// BUY PREMIUM ? // CHANGEPASSWORD //CHANGE MAIL ///
-  const [userNameMenu,setUserNameMenu] = useState<boolean>(false);
-  const [avatarMenu, setAvatarMenu] = useState<boolean>(false);
   const userContext = useContext(UserContext);
-  const [mailMenu,setMailMenu] = useState<boolean>(false);
+  
+  const [userNameInfo,setUserNameInfo] = useState<boolean>(false);
+  const [userNameInput,setUserNameInput] = useState<string>('');
+  const [errorName,setErrorName] = useState<string|null>(null);
+  const [inputNameAnimation , setInputNameAnimation] = useState<string>('');
+  
+  const [mailInfo,setMailInfo] = useState<boolean>(false);
+  const [mailInput,setMailInput] = useState<string>('');
+  const [errorMail,setErrorMail] = useState<string|null>(null);
+  const [inputMailAnimation , setInputMailAnimation] = useState<string>('');
+  
   const [passwordMenu,setPasswordMenu] = useState<boolean>(false);
-  const [color,setColor] = useState<string>(userContext?.user?.userColor ?? '');
+  
   const [premiumMenu,setPremiumMenu] = useState<boolean>(false);
- 
+  
+  const [avatarMenu, setAvatarMenu] = useState<boolean>(false);
+  
+  const [color,setColor] = useState<string>(userContext?.user?.userColor ?? '');
+  
   async function colorHandler(){
     if(userContext?.user){
         const user = userContext.user;
         const updatedUser = {...user, userColor : color}
         userContext.setNewUser(updatedUser);
         await updateUserById(updatedUser);
+    }
+  }
+  async function changeUserNameHandler(){
+    if(userContext?.user){
+      const newUser = {...userContext?.user, userName : userNameInput};
+      try{
+        const nUser = await updateUserById(newUser);
+        if(nUser?.message){
+          userContext?.setNewUser(newUser);
+          setUserNameInfo(true);
+          setTimeout(()=>{
+            setUserNameInfo(false);
+          },4000)
+        }else{
+          setErrorName('userName already taken ! try to be original for once');
+          setInputNameAnimation('shake');
+          setTimeout(()=>{
+            setInputNameAnimation('');
+        },1500)
+        }
+      }catch(error){
+        setErrorName('userName already taken ! try to be original for once');
+        setInputNameAnimation('shake');
+        setTimeout(()=>{
+          setInputNameAnimation('');
+        },1500)
+        return;
+      }
+     
+    }else{
+      setErrorName('You have to login first (i actually don t even know how you get there without !)' )
+    }
+  }
+
+  async function changeMailHandler(){
+    if(userContext?.user){
+      const newUser = {...userContext?.user, mail : mailInput};
+      try{
+        const nUser = await updateUserById(newUser);
+        if(nUser?.message){
+          userContext?.setNewUser(newUser);
+          setMailInfo(true);
+          setTimeout(()=>{
+            setMailInfo(false);
+          },4000)
+        }else{
+          setErrorMail('There is already an account with this mail adress');
+          setInputMailAnimation('shake');
+          setTimeout(()=>{
+            setInputMailAnimation('');
+          },1500)
+        }
+      }catch(error){
+        setErrorMail('There is already an account with this mail adress');
+        setInputMailAnimation('shake');
+        setTimeout(()=>{
+          setInputMailAnimation('');
+        },1500)
+        return;
+      }     
+    }else{
+      setErrorName('You have to login first (i actually don t even know how you get there without !)' )
     }
   }
 
@@ -69,6 +143,8 @@ export function SettingMenu ({onClose} : {onClose : ()=>void}) : JSX.Element{
       return(
               <div className={`overlay ${animation}`} onClick={()=>endwithease()}>
                   <div className={`PopupWithBlurrOption`} onClick={(e)=>e.stopPropagation()}>
+                  <div style={{gridColumn: "1 / -1", display : "flex", maxWidth : "100%" }}>
+                  </div>
 
                     {/* Change userColor part */}
                      <div style={{gridColumn: "1 / -1", display : "flex", maxWidth : "100%" }}>
@@ -79,6 +155,46 @@ export function SettingMenu ({onClose} : {onClose : ()=>void}) : JSX.Element{
                         colorHandler()}} 
                         > 🎨</button>
                       </div>
+                      {/* Change userName part */}
+                     <div style={{gridColumn: "1 / -1", display : "flex", maxWidth : "100%" }}>
+                        <input    
+                        className={`ModernInput ${inputNameAnimation}`}
+                        onChange={(input)=>setUserNameInput(input.target.value)} 
+                        value = {userNameInput}
+                        placeholder="Enter your new userName"/>
+                        <button  
+                        onClick={async ()=>{
+                        changeUserNameHandler();
+                        setUserNameInput('')
+                        }} 
+                        > 🎨</button>
+                      </div>
+                      {errorName && 
+                          <div className="PopupInside" style={{gridColumn: "1 / -1", textAlign :"center" }}>
+                          <span  className="error">{errorName}</span>
+                        </div>
+                      }
+
+                      {/* Change mail part */}
+                      <div style={{gridColumn: "1 / -1", display : "flex", maxWidth : "100%" }}>
+                        <input    
+                        className={`ModernInput ${inputMailAnimation}`}
+                        onChange={(input)=>setMailInput(input.target.value)}
+                        value={mailInput} 
+                        placeholder="Enter your new mailadress"/>
+                        <button  
+                        onClick={async ()=>{
+                          await changeMailHandler();
+                          setMailInput('')
+                          }
+                        } 
+                        > 🎨</button>
+                      </div>
+                      {errorMail && 
+                          <div className="PopupInside" style={{gridColumn: "1 / -1", textAlign :"center" }}>
+                          <span  className="error">{errorMail}</span>
+                        </div>
+                      }
 
                       <button onClick={()=> setAvatarMenu(true)} >Change Avatar</button>
                       {avatarMenu &&
@@ -110,9 +226,9 @@ export function SettingMenu ({onClose} : {onClose : ()=>void}) : JSX.Element{
                       <FriendList 
                       onClose = {()=> setFriendList(false)} 
                       />} */}
-                      {/* <button className="popup-closeOption" 
+                      <button className="popup-closeOption" 
                       style={{gridColumn: "1 / -1", textAlign :"center" }}
-                      onClick={endwithease}>✕</button> */}
+                      onClick={endwithease}>✕</button>
                   </div> 
               </div>
           )
